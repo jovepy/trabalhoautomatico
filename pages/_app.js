adbaf4a73d61822b905be39058b89ffcf93abcb8
Rodrigo Jovê <datajove@gmail.com>
2022-07-19 13:27:16 -0300

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
-import { useEffect } from "react";
+//import { useEffect } from "react";
 
 
 function MyApp({ Component, pageProps }) {
