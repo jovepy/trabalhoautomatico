36705451b37d15ae27662ced4b72882622182d3c
Rodrigo Jovê <datajove@gmail.com>
2022-07-19 19:34:30 -0300

    vaiiii

--- pages/index.js
+++ pages/index.js
@@ -41,27 +41,27 @@ export default function Home() {
             <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabIndex="0">
               comeca aqui
               
-              <div class="row">
-                <div class="col-4">
+              <div className="row">
+                <div className="col-4">
                   <nav id="navbar-example3" class="h-100 flex-column align-items-stretch pe-4 border-end">
-                    <nav class="nav nav-pills flex-column">
-                      <a class="nav-link" href="#item-1">Item 1</a>
-                      <nav class="nav nav-pills flex-column">
-                        <a class="nav-link ms-3 my-1" href="#item-1-1">Item 1-1</a>
-                        <a class="nav-link ms-3 my-1" href="#item-1-2">Item 1-2</a>
+                    <nav className="nav nav-pills flex-column">
+                      <a className="nav-link" href="#item-1">Item 1</a>
+                      <nav className="nav nav-pills flex-column">
+                        <a className="nav-link ms-3 my-1" href="#item-1-1">Item 1-1</a>
+                        <a className="nav-link ms-3 my-1" href="#item-1-2">Item 1-2</a>
                       </nav>
-                      <a class="nav-link" href="#item-2">Item 2</a>
-                      <a class="nav-link" href="#item-3">Item 3</a>
-                      <nav class="nav nav-pills flex-column">
-                        <a class="nav-link ms-3 my-1" href="#item-3-1">Item 3-1</a>
-                        <a class="nav-link ms-3 my-1" href="#item-3-2">Item 3-2</a>
+                      <a className="nav-link" href="#item-2">Item 2</a>
+                      <a className="nav-link" href="#item-3">Item 3</a>
+                      <nav className="nav nav-pills flex-column">
+                        <a className="nav-link ms-3 my-1" href="#item-3-1">Item 3-1</a>
+                        <a className="nav-link ms-3 my-1" href="#item-3-2">Item 3-2</a>
                       </nav>
                     </nav>
                   </nav>
                 </div>
 
-                <div class="col-8">
-                  <div data-bs-spy="scroll" data-bs-target="#navbar-example3" data-bs-smooth-scroll="true" class="scrollspy-example-2" tabindex="0">
+                <div className="col-8">
+                  <div data-bs-spy="scroll" data-bs-target="#navbar-example3" data-bs-smooth-scroll="true" className="scrollspy-example-2" tabindex="0">
                     <div id="item-1">
                       <h4>Item 1</h4>
                       <p>...</p>
