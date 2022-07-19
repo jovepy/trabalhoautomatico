ade6ad86953b0caf56fc65f26cc3dfef77d441a2
Rodrigo Jovê <datajove@gmail.com>
2022-07-19 19:32:13 -0300

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
-
-//import profilePic from '../public/atencaocliente1.gif'
-//import profilePic2 from '../public/atencaocliente2.gif'
-import Image from 'next/image'
+import Image from 'next/image';
+import Router from 'next/router';
 
 const myLoader = ({ src, width, quality }) => {
   return `./${src}?w=${width}&q=${quality || 75}`
@@ -11,6 +9,7 @@ const myLoader = ({ src, width, quality }) => {
 
 export default function Home() {
   return (
+    <div className='bg-white'>
     <div className='contairner-fluid'>
       <div className='row'>
           <div className='col'></div>
@@ -28,23 +27,90 @@ export default function Home() {
           </ul>
           <div className="tab-content" id="pills-tabContent">
             <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabIndex="0">
+              <br></br>
               <div className='row'>
-              <Image loader={myLoader} src='./atencaocliente1.gif' alt="Rosto de Jovepy" width={150} height={150} className="profile"/>
-              <div className='row'>envie o seu processo operacional</div>
+              <Image loader={myLoader} src='./atencaocliente1.gif' alt="Rosto de Jovepy" width={1920} height={1080} className="profile"/>
+              <div className='row'><br></br></div>
               </div>
               <div className='row'>
-              <Image loader={myLoader} src='./atencaocliente2.gif' alt="Rosto de Jovepy" width={150} height={150} className="profile"/>
-              <div className='row'>criamos a solução</div>
+              <Image loader={myLoader} src='./atencaocliente2.gif' alt="Rosto de Jovepy" width={1920} height={1080} className="profile"/>
+              <div className='row'></div>
               </div>
               
             </div>
-            <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabIndex="0">Price</div>
-            <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabIndex="0">contact</div>
+            <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabIndex="0">
+              comeca aqui
+              
+              <div class="row">
+                <div class="col-4">
+                  <nav id="navbar-example3" class="h-100 flex-column align-items-stretch pe-4 border-end">
+                    <nav class="nav nav-pills flex-column">
+                      <a class="nav-link" href="#item-1">Item 1</a>
+                      <nav class="nav nav-pills flex-column">
+                        <a class="nav-link ms-3 my-1" href="#item-1-1">Item 1-1</a>
+                        <a class="nav-link ms-3 my-1" href="#item-1-2">Item 1-2</a>
+                      </nav>
+                      <a class="nav-link" href="#item-2">Item 2</a>
+                      <a class="nav-link" href="#item-3">Item 3</a>
+                      <nav class="nav nav-pills flex-column">
+                        <a class="nav-link ms-3 my-1" href="#item-3-1">Item 3-1</a>
+                        <a class="nav-link ms-3 my-1" href="#item-3-2">Item 3-2</a>
+                      </nav>
+                    </nav>
+                  </nav>
+                </div>
+
+                <div class="col-8">
+                  <div data-bs-spy="scroll" data-bs-target="#navbar-example3" data-bs-smooth-scroll="true" class="scrollspy-example-2" tabindex="0">
+                    <div id="item-1">
+                      <h4>Item 1</h4>
+                      <p>...</p>
+                    </div>
+                    <div id="item-1-1">
+                      <h5>Item 1-1</h5>
+                      <p>...</p>
+                    </div>
+                    <div id="item-1-2">
+                      <h5>Item 1-2</h5>
+                      <p>...</p>
+                    </div>
+                    <div id="item-2">
+                      <h4>Item 2</h4>
+                      <p>...</p>
+                    </div>
+                    <div id="item-3">
+                      <h4>Item 3</h4>
+                      <p>...</p>
+                    </div>
+                    <div id="item-3-1">
+                      <h5>Item 3-1</h5>
+                      <p>...</p>
+                    </div>
+                    <div id="item-3-2">
+                      <h5>Item 3-2</h5>
+                      <p>...</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+            <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabIndex="0">
+            <div className = 'position-absolute top-50 start-50 translate-middle'>
+            <div className='row'></div>
+              <div className='row'>
+              <button className="btn btn-outline-primary" type="button" onClick={() => Router.push('https://api.whatsapp.com/send?phone=+5583998775808&text=Olá!!')}>Envie-me uma mensagem</button>
+
+              </div>
+              <div className='row'></div>
+            </div>
+
+            </div>
           </div>
           </div>
         <div className='col'></div>
     </div>
-    
+    </div>
 </div>
   )
 }
