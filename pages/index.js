8139ecd615f2b176e65f96c00456184bbf67cd26
Rodrigo Jovê <datajove@gmail.com>
2022-07-19 13:44:57 -0300

    agrfoiqvai

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react'
+import React from 'react';
+
 //import profilePic from '../public/atencaocliente1.gif'
 //import profilePic2 from '../public/atencaocliente2.gif'
 import Image from 'next/image'
@@ -10,38 +11,38 @@ const myLoader = ({ src, width, quality }) => {
 
 export default function Home() {
   return (
-    <div class='contairner-fluid'>
-      <div class='row'>
-          <div class='col'></div>
-          <div class='col-6'>
-          <ul class="nav nav-pills nav-fill" id="pills-tab" role="tablist">
-            <li class="nav-item" role="presentation">
-              <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+    <div className='contairner-fluid'>
+      <div className='row'>
+          <div className='col'></div>
+          <div className='col-6'>
+          <ul className="nav nav-pills nav-fill" id="pills-tab" role="tablist">
+            <li className="nav-item" role="presentation">
+              <button className="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
             </li>
-            <li class="nav-item" role="presentation">
-              <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Price</button>
+            <li className="nav-item" role="presentation">
+              <button className="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Price</button>
             </li>
-            <li class="nav-item" role="presentation">
-              <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
+            <li className="nav-item" role="presentation">
+              <button className="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
             </li>
           </ul>
-          <div class="tab-content" id="pills-tabContent">
-            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
-              <div class='row'>
-              <Image loader={myLoader} src='./atencaocliente1.gif' alt="Rosto de Jovepy" width={150} height={150} class="profile"/>
-              <div class='row'>envie o seu processo operacional</div>
+          <div className="tab-content" id="pills-tabContent">
+            <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
+              <div className='row'>
+              <Image loader={myLoader} src='./atencaocliente1.gif' alt="Rosto de Jovepy" width={150} height={150} className="profile"/>
+              <div className='row'>envie o seu processo operacional</div>
               </div>
-              <div class='row'>
-              <Image loader={myLoader} src='./atencaocliente2.gif' alt="Rosto de Jovepy" width={150} height={150} class="profile"/>
-              <div class='row'>criamos a solução</div>
+              <div className='row'>
+              <Image loader={myLoader} src='./atencaocliente2.gif' alt="Rosto de Jovepy" width={150} height={150} className="profile"/>
+              <div className='row'>criamos a solução</div>
               </div>
               
             </div>
-            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">Price</div>
-            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabindex="0">contact</div>
+            <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">Price</div>
+            <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabindex="0">contact</div>
           </div>
           </div>
-        <div class='col'></div>
+        <div className='col'></div>
     </div>
     
 </div>
