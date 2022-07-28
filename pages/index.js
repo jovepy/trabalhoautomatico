64adab431c964d3edf363d396fbeb1a0e2877806
Rodrigo Jovê <datajove@gmail.com>
2022-07-28 19:56:24 -0300

    por hj esta bom

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,9 @@ export default function Home() {
         <div className="card mb-3">
           <div className="row g-0">
             <div className="col-md-4">
-            <Image className="img-fluid rounded-start" loader={myLoader} src='./atencaocliente2.gif' alt="Criamos a sua solulçao" width={480} height={270} />
+            <div className='row'><br></br></div>
+            <div className='row'><br></br></div>
+            <Image className="img-fluid rounded-start" loader={myLoader} src='./atencaocliente2.gif' alt="Criamos a sua solulçao" width={480} height={270} />  
             </div>
             <div className="col-md-8">
               <div className="card-body align-middle ">
@@ -131,14 +133,20 @@ export default function Home() {
 
 
       <footer className='text-center'>
-      <div className="card">
+      <style jsx>{`
+                        svg {
+                          color: white;
+                        } 
+                      `}</style>
+      <div className="card bg-primary">
         <div className="card-body">
           
         <div className='row'><br></br></div>
               <div className="container text-center">
                 <div className='row'>
                   <div className='col'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16" type="button" onClick={() => Router.push('https://www.linkedin.com/in/rodrigojove/')}>
+                    <svg xmlns="http://www.w3.org/2000/svg"  width="60" height="60" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16" type="button" onClick={() => Router.push('https://www.linkedin.com/in/rodrigojove/')}>
+                    
                       <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
                     </svg>
                   </div>
